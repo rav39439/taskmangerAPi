6a2eb351de72aa78ed35f9e9a7dbb4adaae926ec
rav39439 <ravish.kumar@ispf.ngo>
2025-03-07 10:45:29 +0530

    updte

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const conn = mongoose.createConnection(process.env.MONGO_KEY,{ useNewUrlParser: 
 
 
 app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","https://taskmanagerforusers.netlify.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin","https://taskmanagerforusers.netlify.app"); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
